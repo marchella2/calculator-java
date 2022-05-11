31754f8634196a609194e6f30710ad3aa83b32eb
marchella2 <marchella.putris2@gmail.com>
2022-05-11 09:22:04 +0700

    first commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner myObj = new Scanner(System.in); // Scanner object
+
+        System.out.println("=================================== \n" +
+            "Basic Calculator using Java \n" +
+            "=================================== \n" +
+            "Silahkan pilih operasi perhitungan \n" +
+            "1. Penjumlahan \n" +
+            "2. Pengurangan \n" +
+            "3. Perkalian \n" +
+            "4. Pembagian \n"
+        );
+
+//        System.out.println("coba test input");
+//
+//        int number1 = myObj.nextInt();
+//        System.out.println("number 1 = " + number1);
+    }
+}
