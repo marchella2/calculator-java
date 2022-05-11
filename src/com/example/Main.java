106a4a5de93e3fcf1778d384f4644280497ab4af
marchella2 <marchella.putris2@gmail.com>
2022-05-11 10:31:10 +0700

    operators

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -6,8 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Scanner myObj = new Scanner(System.in); // Scanner object
-
         System.out.println("=================================== \n" +
             "Basic Calculator using Java \n" +
             "=================================== \n" +
@@ -18,9 +16,37 @@ public class Main {
             "4. Pembagian \n"
         );
 
-//        System.out.println("coba test input");
-//
-//        int number1 = myObj.nextInt();
-//        System.out.println("number 1 = " + number1);
+        Scanner myObj = new Scanner(System.in); // Scanner object
+        System.out.print("Input operasi yang dipilih (berupa angka)  = ");
+        int operasiPerhitungan = myObj.nextInt();
+        System.out.print("Masukkan angka pertama = ");
+        double angka1 = myObj.nextDouble();
+        System.out.print("Masukkan angka kedua = ");
+        double angka2 = myObj.nextDouble();
+
+        if (operasiPerhitungan == 4 && angka2 == 0.0){
+            System.out.println("Tidak bisa dibagi nol");
+        }
+
+        switch (operasiPerhitungan){
+            case 1:
+                double result  = angka1 + angka2;
+                System.out.println("Hasil penjumlahan dari " + angka1 + " + " + angka2 + " = " + result);
+                break;
+            case 2:
+                result = angka1 - angka2;
+                System.out.println("Hasil pengurangan dari " + angka1 + " - " + angka2 + " = " + result);
+                break;
+            case 3 :
+                result = angka1 * angka2;
+                System.out.println("Hasil perkalian dari " + angka1 + " * " + angka2 + " = " + result);
+                break;
+            case 4 :
+                result = angka1 / angka2;
+                System.out.println("Hasil pembagian dari " + angka1 + " / " + angka2 + " = " + result);
+                break;
+            default:
+                System.out.println("Operasi tidak sesuai");
+        }
     }
 }
